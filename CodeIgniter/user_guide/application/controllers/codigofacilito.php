1c5b27f6a0f279e0d6e6b7c7875357a763eeffdc
ashcrimson <felipepinoredes@gmail.com>
2016-08-26 18:02:47 -0400

    sinelhola

---
+++ CodeIgniter/user_guide/application/controllers/codigofacilito.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Codigofacilito extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+		$this->load->helper('form');
+		$this->load->model('codigofacilito_model');
+	}
+	function index(){
+		$this->load->library('menu',array('Inicio', 'Contacto', 'Cursos', 'Acerca de'));
+		$data['mi_menu'] = $this->menu->construirMenu();
+		$this->load->view('codigofacilito/headers');
+		$this->load->view('codigofacilito/bienvenido',$data);
+	}
+
+	function holaMundo(){
+		$this->load->view('codigofacilito/headers');
+		$this->load->view('codigofacilito/bienvenido');
+	}
+
+	
+
+}
+?>
---
+++ CodeIgniter/user_guide/application/views/cursos/cursos.php
@@ -0,0 +1,13 @@
+<?
+	if($cursos){
+	foreach ($cursos->result() as $curso) { ?>
+		<ul>
+			<li><a href="<?= $curso->idCurso; ?>"><?= $curso->nombreCurso; ?></a></li>
+		</ul>
+	<? } 
+}else {
+		echo "<p>Error en la aplicación</p>";
+		} 
+	?>
+</body>
+</html>
